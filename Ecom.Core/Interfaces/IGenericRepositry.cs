d5a4795310e8d09524371cfaddd27035baa1467e
Ali Mohammed <93075044+AliMohMoh@users.noreply.github.com>
2025-03-05 22:00:01 +0300

    Refactor generic repository interface and implementation

--- Ecom.Core/Interfaces/IGenericRepositry.cs
+++ Ecom.Core/Interfaces/IGenericRepositry.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ecom.Core.Interfaces
+namespace Ecom.Core.Interfaces;
+
+public interface IGenericRepositry<TEntity> where TEntity : class
 {
-    interface IGenericRepositry
-    {
-    }
+    Task<IReadOnlyList<TEntity>> GetAllAsync();
+    Task<IReadOnlyList<TEntity>> GetAllAsync(params Expression<Func<TEntity, object>>[] includes);
+    Task<TEntity> GetByIdAsync(Guid id);
+    Task<TEntity> GetByIdAsync(Guid id, params Expression<Func<TEntity, object>>[] includes);
+    Task AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(Guid id);
 }
--- Ecom.Infrastructure/Repositories/GenericRepositry.cs
+++ Ecom.Infrastructure/Repositories/GenericRepositry.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using Ecom.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ecom.Infrastructure.Repositories
+namespace Ecom.Infrastructure.Repositories;
+
+public class GenericRepositry<TEntity> : IGenericRepositry<TEntity> where TEntity : class
 {
-    class GenericRepositry
+    public Task AddAsync(TEntity entity)
     {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<TEntity>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<TEntity>> GetAllAsync(params Expression<Func<TEntity, object>>[] includes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity> GetByIdAsync(Guid id, params Expression<Func<TEntity, object>>[] includes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
     }
 }
