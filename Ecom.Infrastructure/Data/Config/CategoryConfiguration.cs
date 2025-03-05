b78a5162c8fbea8aeab8f5a3d3b0e8393635dd93
Ali Mohammed <93075044+AliMohMoh@users.noreply.github.com>
2025-03-05 22:12:47 +0300

    Add DbContext and entity configurations for Products and Categories

---
+++ Ecom.Infrastructure/Data/Config/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Ecom.Core.Entities.Product;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Data.Config
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(f=>f.Name).IsRequired().HasMaxLength(30);
+            builder.Property(f => f.Id).IsRequired();
+        }
+    }
+}
---
+++ Ecom.Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Ecom.Core.Entities.Product;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Data.Config;
+
+class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.Property(f => f.Name).IsRequired();
+        builder.Property(f => f.Description).IsRequired();
+        builder.Property(f => f.Price).HasColumnType("decimal(18,2)");
+    }
+}
