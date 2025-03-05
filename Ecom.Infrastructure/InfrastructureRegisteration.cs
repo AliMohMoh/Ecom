1a25e9fba6b85ce3056d8bc466fa48cef394f529
Ali Mohammed <93075044+AliMohMoh@users.noreply.github.com>
2025-03-05 22:48:45 +0300

    Add repository interfaces and implementations for Category, Photo, and Product

---
+++ Ecom.Infrastructure/InfrastructureRegisteration.cs
@@ -0,0 +1,25 @@
+﻿using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure
+{
+    public static class InfrastructureRegisteration
+    {
+        public static IServiceCollection InfrastructureConfiguration(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepositry<>), typeof(GenericRepositry<>));
+            //services.AddScoped<IPhotoRepositry, PhotoRepositry>();
+            //services.AddScoped<IProductRepositry, ProductRepositry>();
+            //services.AddScoped<ICategoryRepositry, CategoryRepositry>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
---
+++ Ecom.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public ICategoryRepositry CategoryRepositry { get; }
+
+    public IPhotoRepositry PhotoRepositry { get; }
+
+    public IProductRepositry ProductRepositry { get; }
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+        CategoryRepositry = new CategoryRepositry(_context);
+        PhotoRepositry = new PhotoRepositry(_context);
+        ProductRepositry = new ProductRepositry(_context);
+    }
+}
